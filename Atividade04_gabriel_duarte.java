72d21f7c1834beeb40525f38b50ddf73ce565936
gbduarteoliveira <gabrieloliveiraduarte14@gmail.com>
2025-09-23 20:51:17 -0300

    Add files via upload

---
+++ Atividade04_gabriel_duarte.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04_yuriand_gabriel;
+import java.util.Scanner;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04_yuriand_gabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+        int n;
+        System.out.println("digite o numero: ");
+       n=entrada.nextInt();
+         if (n < 0) {
+            System.out.println("Esse número é negativo");
+        } else if (n == 0) {
+            System.out.println("Esse número é zero");
+        } else {
+            System.out.println("Esse número é positivo");
+        }
+        
+    }
+    
+}
