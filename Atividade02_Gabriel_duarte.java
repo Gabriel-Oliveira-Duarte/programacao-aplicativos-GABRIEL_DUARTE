62b60c1f8edea1999f4d7252f31ed327e99fec7a
gbduarteoliveira <gabrieloliveiraduarte14@gmail.com>
2025-10-07 09:31:27 -0300

    Update Atividade02_Gabriel_duarte.java

--- Atividade02_Gabriel_duarte.java
+++ Atividade02_Gabriel_duarte.java
@@ -21,7 +21,7 @@ public class Atividade02_Gabriel_duarte {
         //System.out.println("Hello\t World!");
         
    
-      
+      //1
        //double raio,area,circuferencia;
        
        //System.out.println("digite o valor do raio da circuferencia");
@@ -37,7 +37,7 @@ public class Atividade02_Gabriel_duarte {
        
        
         
-        // TODO code application logic here
+        // 2
         //Scanner input=new Scanner(System.in);
        
        //double peso, altura,imc;
@@ -47,7 +47,10 @@ public class Atividade02_Gabriel_duarte {
           //altura= input.nextDouble();
           //imc= peso/(altura*altura);
           //System.out.println("o resultado do imc é:" +imc);
+          
+         //3 
           //Scanner input=new Scanner(System.in);
+          
           //double a, b,c;
           //System.out.println("digite o valor de b");
           //b= input.nextDouble();
@@ -55,7 +58,9 @@ public class Atividade02_Gabriel_duarte {
           //c= input.nextDouble();
           //a= Math.sqrt(Math.pow(b,2)+ Math.pow(c,2));
           //System.out.printf(" o resultado de a é: %.3f",a);
-          
+        
+
+        //4
           //Scanner input=new Scanner(System.in);
           //double real, dolar,resultado, troco;
            //dolar=5.32;
@@ -67,34 +72,76 @@ public class Atividade02_Gabriel_duarte {
         //System.out.println("O valor em dólares e: " + resultado);
         //System.out.println("O troco em reais e: " + troco);
 
-      
-            
+        
+        //5 
+        //Scanner input=new Scanner(System.in);
+          //int n,j1,j2,s;
+          //n=input.nextInt();
+          //j1=input.nextInt();
+          //j2=input.nextInt();
+          //s=(j1+j2)/n;
           
-          
-          
-          
-          
-          
-          
-          
-          
-          
-                  
-          
-          
-          
-          
-          
+          //System.out.print("a média é"+s);
+          //System.out.print(Math.max(j1,j2));
+          //System.out.println(Math.min(j1,j2));
+           
+          //6
+          //Scanner input=new Scanner(System.in);
+         // double graus, tan,sen,cos, rady;
+         // graus=input.nextDouble();
+         // rady=Math.toRadians(graus);
+         // sen=Math.sin(rady);
+         // cos=Math.cos(rady);
+         // tan=Math.tan(rady);
+         // System.out.println("tang"+tan+ "cos"+cos+ "seno"+sen);
          
-          
-         
-                 
+        //7
+         //Scanner input=new Scanner(System.in);
+        // double x1,x2,y1,y2,calc;
+         //x1=input.nextDouble();
+          // x2=input.nextDouble();
+         //  y1=input.nextDouble();
+         //y2=input.nextDouble();
+        // calc=Math.sqrt(Math.pow((x2-x1),2))+ (Math.pow((y2-y1),2));
+        
+        //8
+        
+       // Scanner input=new Scanner(System.in);
+        // double n1,n2,n3,m,s,d;
+        // System.out.println("digite o valor de n1");
+         //n1=input.nextDouble();
+        // System.out.println("digite o valor de n2");
+        // n2=input.nextDouble();
+        // System.out.println("digite o valor de n3");
+        // n3=input.nextDouble();
+         //m=(n1+n2+n3)/3;
+        // s= Math.abs(n1)+ Math.abs(n2)+ Math.abs(n3);
+        // d= Math.abs(n1-n3);
+        //System.out.println("a diferença é"+d);
+ 
+
        
+         //9
+         
+        // int a,b,c;
+        // double x1,x2, delta;
+         //a=input.nextInt();
+        // b=input.nextInt();
+        // c=input.nextInt();
+       
+        //delta=Math.sqrt((Math.pow(b,2))-(4*a*c));
+       // x1=(0-b)+(delta)/(2*a);
+        // x2=(0-b)+(delta)/(2*a);
+        
+        //10
+        //double far,cel;
+       // System.out.print("digite o valor de far:");
+       // far=input.nextDouble();
+        //cel=(far-32)/1.0;
+ //  System.out.println("O valor de " + far + "°F em Celsius é: " + cel + "°C");
+          
        
     }
           
     
 }
-
-
-
